8003e4e5b87c891ccaa3be141960f7de01631380
Thiago Freitas <thiagorbfreitass@gmail.com>
2024-03-04 15:06:33 -0300

    IF TERNÁRIO

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿//----IF Ternário----
+int numero = 0;
+bool ehPar = false;
+
+Console.WriteLine ("Digite um Número:");
+numero = Convert.ToInt32(Console.ReadLine());
+
+ehPar = numero % 2 == 0;
+
+Console.WriteLine($"O número {numero} é " + (ehPar ? "par" : "impar"));
